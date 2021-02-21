99288e9fb79619cde6329f8105556773d4c3376e
Gregory <setantaontherock1@gmail.com>
2021-02-21 18:57:45 +0100

    Improve docstrings

--- find_limit_dataframe.py
+++ find_limit_dataframe.py
@@ -1,5 +1,8 @@
 """
 @author: gpwolfe
+
+Read data files and return data for plotting by find_limit_line.py. Executed
+by find_limit_execute.py
 """
 import os
 import pandas as pd
@@ -7,10 +10,41 @@ import re
 
 
 def limit_data(directory):
+    """
+    Read data files from directory and return dataframe of values to plot.
+
+    File naming should follow the following conventions:
+    Data to be plotted: protein_temperature_80ms
+    (where 80 is replaced by the appropriate time in milliseconds with "ms"
+    appended)
+    Control data: protein_temperature_control
+    Error data (currently not implemented): protein_temperature_80msB
+    (where 80 is replaced by the appropriate time in milliseconds with "msB"
+    appended)
+
+    Example:
+        data, errors = limit_data(my_data_directory)
+
+    Parameters
+    ----------
+    directory : string
+        Filepath to directory containing data files.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        Dataframe containing peak intensity values to be plotted, indexed by
+        time in milliseconds (index) and peak name (columns).
+    errors : pandas.DataFrame
+        Dataframe containing values for setting errorbars on plot.
+
+    """
     dfs = []
     errors = []
+
     for file in os.listdir(directory):
         path = os.path.join(directory, file)
+
         if (not file.startswith('.') and not file.endswith('B')
                 and not file.endswith('control')):
             time = int(re.findall(r'\d+', file.split('_')[-1])[0])
@@ -20,6 +54,7 @@ def limit_data(directory):
             data.columns = [x.strip() for x in data.loc['point']]
             data = data.drop('point')
             dfs.append(data)
+
         if file.endswith('control'):
             control = pd.read_table(path)[['Assign F1', 'Height']]
             control.columns = ["point", 'control']
@@ -35,8 +70,10 @@ def limit_data(directory):
             error.columns = [x.strip() for x in error.loc['point']]
             error = error.drop('point')
             errors.append(error)
+
     df = (pd.concat(dfs).astype('float').divide(control.iloc[0])
           .reset_index(drop=False).rename(columns={'index': 'time'}))
     errors = (pd.concat(errors).astype('float').divide(control.iloc[0])
               .reset_index(drop=False).rename(columns={'index': 'time'}))
+
     return df, errors
--- find_limit_execute.py
+++ find_limit_execute.py
@@ -1,5 +1,23 @@
 """
 @author: gpwolfe
+
+Execute find_limit_dataframe.py and find_limit_line.py.
+
+Plot data from a series of files. Each file represents intensity data from a
+series of peaks for a single time value. For example, one file might include
+intensity data for twelve peaks for time = 20 milliseconds.
+Column name should correspond to peak name; plot title will be based on this.
+
+File naming should follow the following conventions:
+
+Data to be plotted: protein_temperature_80ms
+    (where 80 is replaced by the appropriate time in milliseconds with "ms"
+     appended)
+Control data: protein_temperature_control
+Error data (currently not implemented): protein_temperature_80msB
+    (where 80 is replaced by the appropriate time in milliseconds with "msB"
+     appended)
+
 """
 from argparse import ArgumentParser
 import sys
@@ -7,22 +25,51 @@ import sys
 from find_limit_line import limit_func, plotty
 from find_limit_dataframe import limit_data
 
-directory = "/Users/piper/Code/CLEANEX_experiment_Jones/data_folder"
+
 def execute_limit(directory):
+    """
+    Execute data file reading and plotting with line of best fit.
+
+    Plots will be saved to current working directory.
+
+    Parameters
+    ----------
+    directory : string
+        Filepath to directory containing data files.
+
+    Returns
+    -------
+    None.
+
+    """
     data, errors = limit_data(directory)
     xs = data['time']
 
     for point in data.columns[1:]:
-        plotty(xs, data[point], point)    
+        plotty(xs, data[point], point)
+
 
 def main(argv):
-    parser = ArgumentParser(description = "Plot line of fit for CLEANEX data.")
+    """
+    Run data file reading and plotting from command line.
+
+    Plots are saved to current working directory.
+
+    Example:
+        >>> python3 find_limit_execute.py my_data_directory
+
+    Returns
+    -------
+    None.
+
+    """
+    parser = ArgumentParser(description="Plot line of fit for CLEANEX data.")
     parser.add_argument('directory', metavar="DIRECTORY",
                         help="data directory")
     args = parser.parse_args(argv)
     directory = args.directory
     execute_limit(directory)
-    
-    
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
